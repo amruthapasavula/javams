855a1fc79f4cca601b5c1af7bb6da0014748a435
amruthapasavula <boyaamrutha2@gmail.com>
2022-08-25 16:55:19 +0530

    loginn changes

--- UserLogin_Service/src/main/java/com/ecommerce/login/controller/LoginController.java
+++ UserLogin_Service/src/main/java/com/ecommerce/login/controller/LoginController.java
@@ -7,36 +7,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.ecommerce.login.dto.UserDto;
-import com.ecommerce.login.model.User;
+import com.ecommerce.login.service.LoginService;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 	
 	@Autowired
-	RestTemplate restTemplate;
-	
-	User user;
+	LoginService login;
 	
 	@PostMapping("/user")
 	public ResponseEntity<String> userLogin(@RequestBody UserDto uDto)
 	{
-		user=restTemplate.getForObject("http://USERREGESTRATION-SERVICE/user/email/"+uDto.getEmail(),User.class);
-		if(user==null)
-		{
-			return new ResponseEntity<>("\"Status\": \"User Details Incorrect, Login UnSuccess....\"", HttpStatus.UNAUTHORIZED);
-		}
-		else if(user.getPassword().equals(uDto.getPassword()))
-		{
-			return new ResponseEntity<>("\"Status\": \"Login Successfull....\"", HttpStatus.OK);
-		}
-		else 
-		{
-			return new ResponseEntity<>("\"Status\": \"User Details Incorrect, Login UnSuccess....\"", HttpStatus.UNAUTHORIZED);
-		}
+		return new ResponseEntity<>(login.userLogin(uDto),HttpStatus.OK);
 				
 	}
 
---
+++ UserLogin_Service/src/main/java/com/ecommerce/login/service/LoginService.java
@@ -0,0 +1,34 @@
+package com.ecommerce.login.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.ecommerce.login.dto.UserDto;
+import com.ecommerce.login.model.User;
+
+@Service
+public class LoginService {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	User user;
+	public String userLogin(UserDto uDto)
+	{
+		user=restTemplate.getForObject("http://USERREGESTRATION-SERVICE/user/email/"+uDto.getEmail(),User.class);
+		if(user==null)
+		{
+			return ("\"Status\": \"User Details Incorrect, Login UnSuccess....\"");
+		}
+		else if(user.getPassword().equals(uDto.getPassword()))
+		{
+			return ("\"Status\": \"Login Successfull....\"");
+		}
+		else 
+		{
+			return ("\"Status\": \"User Details Incorrect, Login UnSuccess....\"");
+		}
+	}
+
+}
